19608847adba1dda3f4da5c794a8b7ca43a395a8
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-05 19:57:06 +0530

    added gfg 160 problems repo

---
+++ Day_001 Second Largest/second_largest.py
@@ -0,0 +1,6 @@
+# Second Largest Element in an Array
+
+# Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
+
+# Note: If the second largest element does not exist, return -1.
+
