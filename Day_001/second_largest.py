72cd010d6feb88eb9e496a897c29bf224693a398
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-30 21:38:38 +0530

    solved q-1 of array  second largest element
    Added the Solution class with getSecondLargest method to find the second largest element in an array. Includes example inputs and expected outputs as documentation.

--- Day_001/second_largest.py
+++ Day_001/second_largest.py
@@ -5,3 +5,40 @@
 # Note: If the second largest element does not exist, return -1.
 
 # Solution comming soon!........
+
+# I am Back with the solution
+
+# Examples
+
+"""
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second 
+
+largest element is 34.
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second
+
+largest element is 5.
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 and the second largest element does not exist.
+"""
+
+class Solution:
+    def getSecondLargest(self, arr):
+        first = -1
+        second = -1
+
+        for num in arr: 
+            if num > first:
+                second = first
+                first = num
+            else:
+                if num > second and num < first:
+                    second = num
+
+        return second
