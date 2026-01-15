baa2651e092c97527b020a06cfc01130083d15b6
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-15 22:56:26 +0530

    Add solution for first non-repeating character problem
    Implements a function to find the first non-repeating character in a string. Returns '$' if all characters repeat. Includes example test cases.

---
+++ Day 017/Non Repeating Character.py
@@ -0,0 +1,43 @@
+# Non Repeating Character
+
+# Given a string s consisting of lowercase English Letters. return the first non-repeating character in s. If there is no non-repeating character, return '$'.
+
+# Examples :
+
+"""
+Examples:
+
+Input: s = "geeksforgeeks"
+Output: 'f'
+Explanation: In the given string, 'f' is the first character in the string which does not repeat.
+
+Input: s = "racecar"
+Output: 'e'
+Explanation: In the given string, 'e' is the only character in the string which does not repeat.
+
+Input: s = "aabbccc"
+Output: '$'
+Explanation: All the characters in the given string are repeating.
+
+"""
+
+class Solution:
+    def nonRepeatingChar(self,s):
+        char_count = {}
+     
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+        
+        for char in s:
+            if char_count[char] == 1:
+                return char
+        
+        return '$'
+
+solution = Solution()
+print(solution.nonRepeatingChar("geeksforgeeks"))
+print(solution.nonRepeatingChar("racecar"))        
+print(solution.nonRepeatingChar("aabbccc"))        
