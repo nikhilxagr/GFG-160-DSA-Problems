73766b64bb5ccea3aa96073287a213eac6c316e8
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-10 23:56:50 +0530

    Add permutations solution with backtracking
     implementing Solution.findPermutation. Uses collections.Counter and DFS/backtracking to generate all unique permutations of a string (handles duplicate characters) and returns a list of permutation strings.

---
+++ Day 41 TO 60/Day 043/Permutations of a String.py
@@ -0,0 +1,46 @@
+# Permutations of a String
+
+# Given a string s, which may contain duplicate characters, your task is to generate and return an array of all unique permutations of the string. You can return your answer in any order.
+
+# Examples:
+
+# Input: s = "ABC"
+# Output: ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
+# Explanation: Given string ABC has 6 unique permutations.
+# Input: s = "ABSG"
+# Output: ["ABGS", "ABSG", "AGBS", "AGSB", "ASBG", "ASGB", "BAGS", "BASG", "BGAS", "BGSA", "BSAG", "BSGA", "GABS", "GASB", "GBAS", "GBSA", "GSAB", "GSBA", "SABG", "SAGB", "SBAG", "SBGA", "SGAB", "SGBA"]
+# Explanation: Given string ABSG has 24 unique permutations.
+# Input: s = "AAA"
+# Output: ["AAA"]
+# Explanation: No other unique permutations can be formed as all the characters are same.\
+
+# Solution
+
+
+class Solution:
+    def findPermutation(self, s):
+        from collections import Counter
+        
+        result = []
+        count = Counter(s)
+        path = []
+        
+        def backtrack():
+            if len(path) == len(s):
+                result.append("".join(path))
+                return
+            
+            for char in count:
+                if count[char] > 0:
+                    path.append(char)
+                    count[char] -= 1
+                    
+                    backtrack()
+                    
+                    path.pop()
+                    count[char] += 1
+        
+        backtrack()
+        return result
+
+    
