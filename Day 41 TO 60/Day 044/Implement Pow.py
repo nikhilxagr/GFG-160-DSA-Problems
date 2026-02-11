6776cd8de2fb1b37c087228dae593db311a8fdca
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-11 23:52:23 +0530

    Add Solution.power implementation
    Add new file Implement Pow.py with a Solution.power(b, e) method that computes b raised to the e using Python's exponent operator (b ** e).

---
+++ Day 41 TO 60/Day 044/Implement Pow.py
@@ -0,0 +1,16 @@
+# Implement Pow
+
+# Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+
+# Examples:
+
+# Input: b = 3.00000, e = 5
+# Output: 243.00000
+# Input: b = 0.55000, e = 3
+# Output: 0.16638
+# Input: b = -0.67000, e = -7
+# Output: -16.49971
+
+class Solution:
+    def power(self, b: float, e: int) -> float:
+        return b ** e
