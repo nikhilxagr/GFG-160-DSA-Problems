42877099ed1bbf616ead8599408477589029ff04
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-04-14 23:59:56 +0530

    Use frequency map to find first repeated char
    Refactor firstRep to build a frequency map in a first pass and then return the first character with count > 1. Also cleans up minor whitespace/newline issues.

--- Day 101 TO 120/Day 106/Repeated Character.py
+++ Day 101 TO 120/Day 106/Repeated Character.py
@@ -24,11 +24,12 @@ class Solution:
     def firstRep(self, s):
         # code here
         char_count = {}
-        
+
         for char in s:
-            if char in char_count:
+            char_count[char] = char_count.get(char, 0) + 1
+
+        for char in s:
+            if char_count[char] > 1:
                 return char
-            else:
-                char_count[char] = 1
-        
+
         return '#'
