73acbddc609b052f0af40f33fcdfad1bd446806f
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-06-01 15:28:40 +0530

    Add sum of n natural numbers
    Introduce a new Python file with Solution.utility(x) that prints integers from x down to 0 (inclusive) on a single line separated by spaces.

---
+++ Logic Based Problems/sum of n natural numbers.py
@@ -0,0 +1,17 @@
+# sum of n natural numbers
+
+# Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
+
+# Example:
+
+# Input: x = 3
+# Output: 3 2 1 0
+# Explanation: Numbers in decreasing order from 3 are 3 2 1 0.
+# Input: x = 5
+# Output: 5 4 3 2 1 0
+# Explanation: Numbers in decreasing order from 5 are 5 4 3 2 1 0.
+
+class Solution:
+    def utility(self, x):
+        for i in range(x, -1, -1):
+            print(i, end=" ")
