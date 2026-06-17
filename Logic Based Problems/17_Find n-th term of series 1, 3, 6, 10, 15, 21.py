a072d2865ac1e910a10f1bf9035799205c8b61f3
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-06-17 15:51:17 +0530

    solve the Find n-th term of series
    Approach:
    The given series is the series of triangular numbers. The nth triangular number can be calculated using
    the formula T(n) = n * (n + 1) / 2. This formula gives the nth term of the series directly, making it efficient to compute the result in constant time O(1).

---
+++ Logic Based Problems/17_Find n-th term of series 1, 3, 6, 10, 15, 21.py
@@ -0,0 +1,16 @@
+# Find n-th term of series 1, 3, 6, 10, 15, 21
+
+# Given a number n, find the nth term in the series 1, 3, 6, 10, 15, 21…
+
+# Examples:
+
+# Input: n = 4 
+# Output: 10
+# Explanation: The 4th term of the Series is 10.
+# Input: n = 3 
+# Output: 6
+# Explanation: The 3rd term of the Series is 6.
+
+class Solution:
+    def findNthTerm(self, n):
+        return (n * (n + 1)) // 2
