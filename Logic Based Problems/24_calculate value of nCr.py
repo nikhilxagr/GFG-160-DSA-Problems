56d7ac31c25ef9e01d7f7eb193f3c61ec24b6513
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-06-24 22:33:40 +0530

    solved calculate value of nCr
    Approach- use factorial function to calculate nCr using the formula nCr = n! / (r! * (n - r)!). If r is greater than n, return 0 since nCr is not defined in that case.

---
+++ Logic Based Problems/24_calculate value of nCr.py
@@ -0,0 +1,40 @@
+# calculate value of nCr
+
+# Given two integer values n and r, the task is to find the value of Binomial Coefficient nCr
+
+# A binomial coefficient nCr can be defined as the coefficient of xr in the expansion of (1 + x)n that gives the number of ways to choose r objects from a set of n objects without considering the order.
+# The binomial coefficient nCr is calculated as : C(n,r) = n! / r! * (n-r) !
+# Note: If r is greater than n, return 0.
+# It is guaranteed that the value of nCr will fit within a 32-bit integer.
+
+# Examples:
+
+# Input: n = 5, r = 2
+# Output: 10
+# Explaination: The value of 5C2 is calculated as 5!/(5−2)!*2! = 5!/3!*2! = 10.
+# Input: n = 2, r = 4
+# Output: 0
+# Explaination: Since r is greater than n, thus 2C4 = 0
+# Input: n = 5, r = 0
+# Output: 1
+# Explaination: The value of 5C0 is calculated as 5!/(5−0)!*0! = 5!/5!*0! = 1.
+
+class Solution:
+    def nCr(self, n, r):
+        # calculate factorial
+        
+        def factorial(num):
+            if num == 0 or num == 1:
+                return 1
+            else:
+                return num * factorial(num - 1)
+
+        # Calculate nCr using the formula nCr = n! / (r! * (n - r)!)
+        
+        if r > n:
+            return 0    # If r is greater than n, nCr is not defined
+        
+        else:
+            return factorial(n) // (factorial(r) * factorial(n - r))
+        
+# Approach- use factorial function to calculate nCr using the formula nCr = n! / (r! * (n - r)!). If r is greater than n, return 0 since nCr is not defined in that case.
